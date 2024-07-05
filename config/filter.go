7a752912bce7ee51c31fe33b23c29bae5bb6a999
Nico Franzeck <nico.franzeck@cloudogu.com>
2024-07-05 15:29:03 +0200

    add comments to filter.go

--- config/filter.go
+++ config/filter.go
@@ -2,8 +2,11 @@ package config
 
 import "strings"
 
+// WatchFilter can be applied to the result of a comparison of two configurations.
+// It is a predicate function that should to return true in case the filter matches.
 type WatchFilter func([]DiffResult) bool
 
+// KeyFilter is a WatchFilter to watch for changes for a single Key.
 func KeyFilter(k Key) WatchFilter {
 	k = sanitizeKey(k)
 
@@ -18,6 +21,13 @@ func KeyFilter(k Key) WatchFilter {
 	}
 }
 
+// DirectoryFilter is a WatchFilter to watch for changes for a directory.
+// This can encompass multiple keys with the same prefix.
+// For example:
+// Filter with Key "key1/" watch for changes for the keys:
+// key1/key2/key3
+// key1/key2/key3
+// ...
 func DirectoryFilter(directory Key) WatchFilter {
 	directory = sanitizeKey(directory)
 	if directory != "" && !strings.HasSuffix(directory.String(), keySeparator) {
