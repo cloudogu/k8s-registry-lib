85694607ee7d33a9427fc920d39ebfe761201c4d
Jeremias Weber <jeremias.weber@cloudogu.com>
2024-08-30 09:48:28 +0200

    clean up and fix interfaces

--- dogu/interfaces.go
+++ dogu/interfaces.go
@@ -7,22 +7,6 @@ import (
 	"github.com/cloudogu/cesapp-lib/core"
 )
 
-// LocalRegistry abstracts accessing various backends for reading and writing dogu specs (dogu.json).
-type LocalRegistry interface {
-	// Enable makes the dogu spec reachable.
-	Enable(ctx context.Context, dogu *core.Dogu) error
-	// Register adds the given dogu spec to the local registry.
-	Register(ctx context.Context, dogu *core.Dogu) error
-	// UnregisterAllVersions deletes all versions of the dogu spec from the local registry and makes the spec unreachable.
-	UnregisterAllVersions(ctx context.Context, simpleDoguName string) error
-	// GetCurrent retrieves the spec of the referenced dogu's currently installed version.
-	GetCurrent(ctx context.Context, simpleDoguName string) (*core.Dogu, error)
-	// GetCurrentOfAll retrieves the specs of all dogus' currently installed versions.
-	GetCurrentOfAll(ctx context.Context) ([]*core.Dogu, error)
-	// IsEnabled checks if the current spec of the referenced dogu is reachable.
-	IsEnabled(ctx context.Context, simpleDoguName string) (bool, error)
-}
-
 type configMapClient interface {
 	corev1client.ConfigMapInterface
 }
@@ -58,7 +42,7 @@ type CurrentVersionsWatchResult struct {
 // LocalDoguDescriptorRepository is an append-only Repository, no updates will happen
 type LocalDoguDescriptorRepository interface {
 	Get(context.Context, DoguVersion) (*core.Dogu, error)
-	GetAll(context.Context, []DoguVersion) map[DoguVersion]*core.Dogu
+	GetAll(context.Context, []DoguVersion) (map[DoguVersion]*core.Dogu, error)
 	Add(context.Context, SimpleDoguName, *core.Dogu) error
 	DeleteAll(context.Context, SimpleDoguName) error
 }
